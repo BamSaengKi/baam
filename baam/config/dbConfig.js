8ddae0ae3aac77d26033b5a87dd68a94c5c1fa29
Bam <bamsaengki@gmail.com>
2021-11-27 15:09:08 +0900

    sex

---
+++ baam/config/dbConfig.js
@@ -0,0 +1,16 @@
+const { createPool } = require('mysql2/promise');
+// 운영환경 설정
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const dbinfo = {
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+};
+
+const pool = createPool(dbinfo);
+
+module.exports = pool;
--- baam/routes/index.js
+++ baam/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../config/dbConfig');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '야발' });
+  try {
+    pool.getConnection((err, conn) => {
+      if (err) {
+        console.log(err);
+        throw err.message;
+      }
+      const query = 'SELECT * FROM t_user;';
+      conn.query(query, (err, rows) => {
+        if (err) {
+          console.log(err);
+          throw err.message;
+        }
+        conn.release();
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ status: 500, message: error });
+  }
 });
 
 module.exports = router;
